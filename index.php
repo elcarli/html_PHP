4290b3f31f9024c7a55c190784262fc388ef6f39
Carlos Arrieta <carrietatapia@gmail.com>
2019-01-08 00:41:36 -0500

    Ya cuenta

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<?php include ("includes/header.php") ?>
+<?php 
+  include ("includes/header.php") ;
+  session_start();
+  if(isset($_SESSION['contador'])){
+    echo $_SESSION['contador'];
+  }
+  
+?>
 
         <h1>BIENVENIDO</h1>
         <p id="info">Ingrese un número entre 0 y 100</p>
         <form action="respuesta.php" method="post">
           <div id="divvalor">
             <label>número: <input type="number" name="numero" maxlength="3" min="0" max="100"></label>
+            <input type="hidden" name="tipo" value="1">
           </div>
           <div id="divboton">
             <button type="submit">Enviar</button>
--- respuesta.php
+++ respuesta.php
@@ -1,42 +1,74 @@
 <?php
   include("db.php");
+  session_start();
 
-  $numero = $_POST['numero'];
+  $tipo = $_POST['tipo'];
 
-  if ($numero<77){
-    header("Location: muybajo.php");
-  }
-  else if($numero>77){
-    header("Location: muyalto.php");
-  }
-  else {
-    include ("includes/header.php");
-    ?>
-    <figure id="imgleft">
-      <img src="images/2.png">
-    </figure>
-      <div id="mensaje">
-        <div class="motivacion">
-          <p>¡Felicitaciones!, has adivinado el número.</p>
-          <p>Ingrese su nombre</p>
+  if ($tipo==1) //formulario de intentos
+  {
+    
+    if(isset($_SESSION['contador'])){
+      $_SESSION['contador']++;
+    } else {
+      $_SESSION['contador']=1;
+    }
+
+    $numero = $_POST['numero'];
+
+
+    if ($numero<77){
+      header("Location: muybajo.php");
+    }
+    else if($numero>77){
+      header("Location: muyalto.php");
+    }
+    else if($numero==77) {
+      include ("includes/header.php");
+?>
+      <figure id="imgleft">
+        <img src="images/2.png">
+      </figure>
+        <div id="mensaje">
+          <div class="motivacion">
+            <p>¡Felicitaciones!, has adivinado el número.</p>
+            <p>Ingrese su nombre</p>
+          </div>
+          <form action="respuesta.php" method="post">
+            <div id="divvalor">
+              <input type="hidden" name="tipo" value="2">
+              <label>Nombre: <input type="text" name="nombre" maxlength="20"></label>             
+            </div>
+            <div id="divboton">
+              <button type="submit">Guardar</button>
+            </div>
+        </form>
         </div>
-        <form action="respuesta.php" method="post">
-          <div id="divvalor">
-            <label>Nombre: <input type="text" name="nombre" maxlength="20"></label>
-          </div>
-          <div id="divboton">
-            <button type="submit">Guardar</button>
-          </div>
-      </form>
-      </div>
+  
+      <figure id="imgright">
+        <img src="images/7.png">
+      </figure>
+  
+<?php
 
-    <figure id="imgright">
-      <img src="images/7.png">
-    </figure>
+  include ("includes/footer.php");
+    }
+  } else if ($tipo==2){ //formulario de registro
+  
+    //si se envió el formulario, se registra la puntuacion en la BD con ayuda del ORM RedBean
+    if(isset($_POST['nombre'])){
 
-    <?php
-    include ("includes/footer.php");
+    $puntuacion = R::dispense('puntuaciones');
+    $puntuacion->nombre = $_POST['nombre'];
+    $puntuacion->intentos = $_SESSION['contador'];
 
+    R::store($puntuacion);
+
+    //se elimina la variable del contador para nuevos usuarios que jueguen
+    unset($_SESSION['contador']);
+
+    //Se redirecciona al usuario al index
+    header("Location: index.php");
+    }
   }
-
+  
 ?>
